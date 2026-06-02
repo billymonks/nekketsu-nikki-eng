c3e2fd8ef6bbbdef7d75d7adc4e64b48b8138a4c
Billy Monks <williamtmonks@gmail.com>
2026-06-02 00:19:35 -0400

    Updated rebuild script for image replacements

--- scripts/rebuild.bat
+++ scripts/rebuild.bat
@@ -5,8 +5,12 @@ python .\replace_text.py
 
 cd /d "%~dp0\.."
 
+echo === Staging text patches for MGDATA repack ===
+copy /y modified-afs-contents\MGDATA\00000062 modified-images\mgdata_62.bin >nul
+copy /y modified-afs-contents\MGDATA\00000063 modified-images\mgdata_63.bin >nul
+
 echo === Rebuilding MGDATA.AFS ===
-tools\AFSPacker.exe -c modified-afs-contents\MGDATA modified-disc-files\MGDATA.AFS
+tools\mgrepack_v0.9.0\mgrepack.exe repack -extract extracted-images -replacement modified-images -out modified-disc-files\MGDATA.AFS
 
 echo.
 echo === Rebuilding GDI ===
